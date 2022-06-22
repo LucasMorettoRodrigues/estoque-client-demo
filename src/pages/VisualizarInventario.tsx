3c199c7c95f8a71659c823616e46c79373df04ba
Lucas <r.lucasmoretto@gmail.com>
2022-06-21 21:29:20 -0300

    clean code

--- src/pages/VisualizarInventario.tsx
+++ src/pages/VisualizarInventario.tsx
@@ -1,9 +1,9 @@
 import styled from "styled-components"
 import { useLocation, useNavigate } from "react-router-dom"
-import { useAppDispatch, useAppSelector } from "../app/hooks"
-import { useEffect, useState } from "react"
+import { useAppDispatch } from "../app/hooks"
+import { useState } from "react"
 import { TProduct } from "../types/TProduct"
-import { compare, formatValidity } from "../utils/functions"
+import { formatValidity } from "../utils/functions"
 import ListHeader from "../components/List/ListHeader"
 import Item from "../components/List/Item"
 import ItemsContainer from "../components/List/ItemsContainer"
@@ -40,42 +40,9 @@ export default function VizualizarInventario() {
     const dispatch = useAppDispatch()
     const navigate = useNavigate()
 
-    const productsData = state.data
-    const providerFilter = useAppSelector(state => state.produto.providerFilter)
-    const categoryFilter = useAppSelector(state => state.produto.categoryFilter)
-    const searchFilter = useAppSelector(state => state.produto.searchFilter)
+    const products: TProduct[] = state.data
 
-    const [filteredProducts, setFilteredProducts] = useState<TProduct[]>([])
-    const [sortedProducts, setSortedProducts] = useState<TProduct[]>([])
-    const [sort, setSort] = useState('')
     const [message, setMessage] = useState<TMessage>()
-
-
-    useEffect(() => {
-        let products = productsData.slice()
-
-        sort ? setSortedProducts(compare(products, sort)) : setSortedProducts(products)
-
-    }, [sort, productsData])
-
-    useEffect(() => {
-        let filtered = sortedProducts
-
-        if (categoryFilter) {
-            filtered = filtered.filter(i => i.category === categoryFilter)
-        }
-
-        if (providerFilter) {
-            filtered = filtered.filter(i => i.providers?.includes(providerFilter))
-        }
-
-        if (searchFilter) {
-            filtered = filtered.filter(i => i.name.toLowerCase().includes(searchFilter))
-        }
-
-        setFilteredProducts(filtered)
-
-    }, [categoryFilter, sortedProducts, searchFilter, providerFilter])
 
     const handleArchive = async () => {
         try {
@@ -110,19 +77,19 @@ export default function VizualizarInventario() {
             <h3 style={{ marginTop: '30px', marginBottom: '10px' }}>Produtos com Divergência</h3>
             <HeaderContainer>
                 <ListHeader fontSize='12px'>
-                    <Item width="26px" text='Id' onClick={() => setSort('id')} cursor='pointer' fontSize='12px' />
-                    <Item flex={3} text='Produto' onClick={() => setSort('name')} cursor='pointer' fontSize='12px' />
+                    <Item width="26px" text='Id' fontSize='12px' />
+                    <Item flex={3} text='Produto' fontSize='12px' />
                     <Item flex={2} text='Observação' fontSize='12px' />
                     <Item width="90px" text='Código' fontSize='12px' />
-                    <Item width="90px" text='Categoria' onClick={() => setSort('category')} cursor='pointer' fontSize='12px' />
-                    <Item width="130px" text='Marca' onClick={() => setSort('brand')} cursor='pointer' fontSize='12px' />
-                    <Item width="65px" text='Unidade' onClick={() => setSort('unit')} cursor='pointer' fontSize='12px' />
+                    <Item width="90px" text='Categoria' fontSize='12px' />
+                    <Item width="130px" text='Marca' fontSize='12px' />
+                    <Item width="65px" text='Unidade' fontSize='12px' />
                     <Item width="65px" text='Estoque' align='center' fontSize='12px' />
                 </ListHeader>
             </HeaderContainer>
 
             {
-                filteredProducts
+                products
                     .map(i => ({ ...i, subproducts: i.subproducts?.filter(j => j.justification) }))
                     .filter(i => i.subproducts!.length > 0)
                     .map((item) => (
@@ -171,7 +138,7 @@ export default function VizualizarInventario() {
             <h3 style={{ marginTop: '40px', marginBottom: '10px' }}>
                 Inventário completo
             </h3>
-            <InventarioList products={filteredProducts} />
+            <InventarioList products={products} />
         </>
     )
 }
