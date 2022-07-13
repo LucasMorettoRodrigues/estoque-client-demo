7a85d4cf291b24029b416486cb4a0dc90cb7a90b
Lucas <45830155+LucasMorettoRodrigues@users.noreply.github.com>
2022-07-13 13:48:29 -0300

    Update api.service.ts

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { getUser } from './auth.service';
 
 export const api = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL || "https://estoque-8.herokuapp.com/"
+    baseURL: process.env.REACT_APP_BASE_URL || "https://estoque-8.herokuapp.com/api/v1"
 })
 
 api.interceptors.request.use(async config => {
